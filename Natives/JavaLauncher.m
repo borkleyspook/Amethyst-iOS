3615bef0e752c0c794c60ac2cce62721cd7231d6
Borkleyspook <115428985+borkleyspook@users.noreply.github.com>
2025-09-16 11:40:19 -0700

    Add Ely.by support without updating makefile

--- Natives/JavaLauncher.m
+++ Natives/JavaLauncher.m
@@ -15,6 +15,8 @@
 #import "JavaLauncher.h"
 #import "LauncherPreferences.h"
 #import "PLProfiles.h"
+#import "authenticator/BaseAuthenticator.h"
+#import "authenticator/ElyByAuthenticator.h"
 
 #define fm NSFileManager.defaultManager
 
@@ -220,6 +222,23 @@ int launchJVM(NSString *username, id launchTarget, int width, int height, int mi
     margv[++margc] = [NSString stringWithFormat:@"-javaagent:%@/patchjna_agent.jar=", librariesPath].UTF8String;
     if(getPrefBool(@"general.cosmetica")) {
         margv[++margc] = [NSString stringWithFormat:@"-javaagent:%@/arc_dns_injector.jar=23.95.137.176", librariesPath].UTF8String;
+    }
+
+    // Add authlib-injector arguments if ely.by account is used
+    if ([username length] > 0 && [BaseAuthenticator.current isKindOfClass:ElyByAuthenticator.class]) {
+        NSDictionary *authData = BaseAuthenticator.current.authData;
+        if (authData[@"authserver"] != nil) {
+            NSLog(@"[JavaLauncher] Adding authlib-injector arguments for ely.by");
+            NSArray *authlibArgs = [(ElyByAuthenticator *)BaseAuthenticator.current getJvmArgsForAuthlib];
+            if (authlibArgs.count > 0) {
+                for (NSString *arg in authlibArgs) {
+                    margv[++margc] = arg.UTF8String;
+                    NSLog(@"[JavaLauncher] Added authlib-injector arg: %s", arg.UTF8String);
+                }
+            } else {
+                NSLog(@"[JavaLauncher] Warning: No authlib-injector arguments available");
+            }
+        }
     }
 
     // Workaround random stack guard allocation crashes
